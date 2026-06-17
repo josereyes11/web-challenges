88c17d4b3c52b6180b67e3e8c045210bd5435bf1
Jose Reyes <jose_reyes@outlook.de>
2026-06-17 12:04:25 +0200

    Tip Calculator

---
+++ js-basics-and-variables_tip-calculator/index.js
@@ -0,0 +1,29 @@
+console.clear();
+
+/*
+Create a program that calculates the total cost of a meal, including tip, 
+based on the cost of the meal and the percentage of the tip.
+
+1. Define two variables:
+   - A variable that stores the value of the meal cost.
+   - A variable that stores the value of the tip percentage.
+
+2. Calculate the tip amount by multiplying the meal cost by the tip percentage, and store the result in a variable called `tipAmount`.
+
+3. Calculate the total cost of the meal, including tip, by adding `mealCost` and `tipAmount`, and store the result in a variable called `totalCost`.
+
+4. Log a message to the console that includes the meal cost, tip amount, and total cost.
+*/
+
+let mealCost = 18;
+const tip = 0.15;
+
+let tipAmount = mealCost * tip;
+// console.log(tip);
+
+let totalCost = mealCost + tipAmount;
+// console.log(totalCost);
+
+console.log("Meal cost", mealCost, "€");
+console.log("Tip amount", tipAmount, "€");
+console.log("Total cost", totalCost, "€");
