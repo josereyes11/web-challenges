38f797fa81a5970f8794ff4cfa65cf3e11bed378
Jose Reyes <jose_reyes@outlook.de>
2026-06-22 16:10:17 +0200

    input to uppercase

---
+++ js/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -0,0 +1,10 @@
+console.clear();
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const buttonUppercase = document.querySelector('[data-js="button-uppercase"]');
+
+buttonUppercase.addEventListener("click", () => {
+  const oldValue = firstInput.value;
+  const newValue = oldValue.toUpperCase();
+  firstInput.value = newValue;
+});
