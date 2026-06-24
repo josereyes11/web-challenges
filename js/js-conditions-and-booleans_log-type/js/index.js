56cfe5da24dbffb619b38723374e6cb04a250a83
Jose Reyes <jose_reyes@outlook.de>
2026-06-24 12:43:53 +0200

    logtype

---
+++ js/js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,33 @@
+const data = true;
+
+switch (true) {
+  case Number.isNaN(data):
+    console.log("not a number!");
+    break;
+  case Array.isArray(data):
+    console.log("array!");
+    break;
+  case typeof data === "number":
+    console.log("number!");
+    break;
+  case typeof data === "string":
+    console.log("string!");
+    break;
+  case typeof data === "boolean":
+    console.log("boolean!");
+    break;
+  case data === null:
+    console.log("null");
+    break;
+  case typeof data === "object":
+    console.log("object!");
+    break;
+  case typeof data === "function":
+    console.log("function!");
+    break;
+  case data === undefined:
+    console.log("undefined!");
+    break;
+  default:
+    console.log("I have no idea!");
+}
