939e8a5a4d3a32ab1ed0c7e1d40b2c8eeae923c8
Jose Reyes <jose_reyes@outlook.de>
2026-06-24 17:25:26 +0200

    functions with parameters

--- js/js-functions_functions-with-parameters/js/index.js
+++ js/js-functions_functions-with-parameters/js/index.js
@@ -1,25 +1,25 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+logSeparator(1);
+
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? 
    Replace the function calls below with your new function.
 */
 
+/*
 function greetBob() {
   console.log("Welcome Bob, good to see you again!");
 }
@@ -35,15 +35,23 @@ function greetMary() {
 greetAlice();
 greetBob();
 greetMary();
-
+*/
 // --v-- write your code here --v--
 
-function greet() {}
+logSeparator(2);
+
+function greet(name) {
+  console.log("Welcome " + name + "," + " good to see you again!");
+}
+
+greet("Bob");
+greet("Alice");
+greet("Mary");
 
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
+logSeparator(3);
+
 /*
 3: We log a small separation text for each subtask. This is repetitive code, so let’s write a function `logSeparator` that takes the number of the exercise as an input parameter and logs the corresponding separator.
    Then, use this function to replace the existing separator logs in this JavaScript file.
@@ -52,5 +60,10 @@ Note: Separation text refers to the text that is logged in the console before ea
 */
 
 // --v-- write your code here --v--
+
+function logSeparator(exerciseNumber) {
+  console.log("_____________");
+  console.log("Exercise " + exerciseNumber + ":");
+}
 
 // --^-- write your code here --^--
