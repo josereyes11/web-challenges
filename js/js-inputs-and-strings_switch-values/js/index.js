c1d54a192b43755d56f48a4e1ab56d8570bb4c83
Jose Reyes <jose_reyes@outlook.de>
2026-06-26 15:06:34 +0200

    switch values between two inputs

--- js/js-inputs-and-strings_switch-values/js/index.js
+++ js/js-inputs-and-strings_switch-values/js/index.js
@@ -1,8 +1,13 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchCaseButton = document.querySelector(
+  '[data-js="button-switch-case"]',
+);
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -10,4 +15,10 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchCaseButton.addEventListener("click", () => {
+  const temp = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = temp;
 });
