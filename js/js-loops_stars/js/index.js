e4fbfa4c1632c1a3fddbf1bd4ca68a77d2c58368
Jose Reyes <jose_reyes@outlook.de>
2026-07-11 17:21:34 +0200

    Loops: rating with stars

--- js/js-loops_stars/js/index.js
+++ js/js-loops_stars/js/index.js
@@ -2,13 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
-  // Reset the star container before re-rendering stars
+function renderStars(filledStars) {
   starContainer.innerHTML = "";
 
-  // --v-- write or modify code below this line --v--
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    img.src = "assets/star-empty.svg";
 
-  // --^-- write or modify code above this line --^--
+    if (i <= filledStars) {
+      img.src = "assets/star-filled.svg";
+    } else {
+      img.src = "assets/star-empty.svg";
+    }
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(img);
+  }
 }
 
 renderStars();
