16c6b8d6bfb90e37bffbf78ef5a4a6d13dbdf8ec
Jose Reyes <jose_reyes@outlook.de>
2026-07-14 11:25:48 +0200

    test navigation 2

--- react/react-nesting_navigation/src/App.jsx
+++ react/react-nesting_navigation/src/App.jsx
@@ -1,10 +1,10 @@
 import "./styles.css";
 
-import { Header } from "./components/Header.jsx";
-import { Avatar } from "./components/Avatar.jsx";
-import { Logo } from "./components/Logo.jsx";
-import { Navigation } from "./components/Navigation.jsx";
-import { Link } from "./components/Link.jsx";
+import Header from "./components/Header.jsx";
+import Avatar from "./components/Avatar.jsx";
+import Logo from "./components/Logo.jsx";
+import Navigation from "./components/Navigation.jsx";
+import Link from "./components/Link.jsx";
 
 export default function App() {
   return (
