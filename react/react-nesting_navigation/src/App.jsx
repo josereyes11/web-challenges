1e351aa0a1f74f9b19b73962e53f6565474afa92
Jose Reyes <jose_reyes@outlook.de>
2026-07-14 11:22:30 +0200

    test

---
+++ react/react-nesting_navigation/src/App.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+import { Header } from "./components/Header.jsx";
+import { Avatar } from "./components/Avatar.jsx";
+import { Logo } from "./components/Logo.jsx";
+import { Navigation } from "./components/Navigation.jsx";
+import { Link } from "./components/Link.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header>
+        <Logo />
+        <Navigation>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Navigation>
+        <Avatar />
+      </Header>
+      <main>content goes here…</main>
+    </>
+  );
+}
