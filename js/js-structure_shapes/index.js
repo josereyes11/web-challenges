4906d23583d5d613930bd07c914a33a5baf3bbe1
Jose Reyes <jose_reyes@outlook.de>
2026-07-15 15:51:35 +0200

    JS Structure: shapes

--- js/js-structure_shapes/index.js
+++ js/js-structure_shapes/index.js
@@ -1,29 +1,13 @@
 console.clear();
 
-import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+import Square from "./components/Square/Square.js";
 
 const root = document.getElementById("root");
 
-function Circle() {
-  const circle = document.createElement("div");
-  circle.classList.add("circle");
-  circle.addEventListener("click", () => {
-    circle.style.backgroundColor = getRandomColor();
-  });
-  return circle;
-}
-
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
-
 const circleElement = Circle();
-root.append(circleElement, square, pentagon);
+const squareElement = Square();
+const pentagonElement = Pentagon();
+
+root.append(circleElement, squareElement, pentagonElement);
