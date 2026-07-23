29249087c40e244f06d826dcdc15bef6db768a74
Jose Reyes <jose_reyes@outlook.de>
2026-07-23 17:45:56 +0200

    Challenge favorite holidays

---
+++ react/react-state-2_favourite-holiday/src/App.jsx
@@ -0,0 +1,47 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    setHoliday(event.target.elements.holiday.value);
+    setDate(event.target.elements.date.value);
+    event.target.reset();
+    event.target.elements.holiday.focus();
+  }
+
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
+  return (
+    <div className="container">
+      <h1>Favourite Holiday Data Storage</h1>
+      <h2 id="favourite-holiday">Please tell us your favourite holiday!</h2>
+      <form
+        className="form"
+        aria-labelledby="favourite-holiday"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="holiday">My Favourite Holiday: </label>
+        <input
+          id="holiday"
+          type="text"
+          name="holiday"
+          placeholder="e.g. Christmas"
+        />
+        <label htmlFor="date">Date: </label>
+        <input id="date" type="date" name="date" />
+        <button type="submit" className="form__submit-button">
+          Submit
+        </button>
+      </form>
+      <h2>Output of Submitted Data</h2>
+      <p>
+        Favourite Holiday: <span className="output">{holiday}</span>
+      </p>
+      <p>
+        Date: <span className="output">{date}</span>
+      </p>
+    </div>
+  );
+}
